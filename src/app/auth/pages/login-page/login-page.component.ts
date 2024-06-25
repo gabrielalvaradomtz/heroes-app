dae52e066101d42e5baf32a555ca8ae2f891d215
Gabriela Lizbeth Alvarado Martinez <gabrielalizbethalvaradomartinez@MacBook-Pro-de-Gabriela.local>
2024-06-25 10:32:35 -0600

    Se agrega login y logout.

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.services';
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+
+  constructor( 
+    private authService: AuthService,
+    private router:Router 
+  ) {}
+
+  onLogin(): void {
+    this.authService.login('fernando@gmail.com', '123456')
+      .subscribe( user => {
+
+        this.router.navigate(['/']);
+
+      });
+
+  }
 
 }
